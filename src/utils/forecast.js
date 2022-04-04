00339f0f8e1cbb950b86e9a4960a3f3c2c8aa1b4
Nilotpal <nilotpalraj820@gmail.com>
2022-04-04 14:01:45 +0530

    Added new features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude,longitude,callback) => {
         }else if(body.error){
             callback('Unable to find location',undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+ '. It is currently '+body.current.temperature+ ' degrees out.It feels like '+ body.current.feelslike+ ' degrees out.')
+            console.log(body.current)
+            callback(undefined,body.current.weather_descriptions[0]+ '. It is currently '+body.current.temperature+ ' degrees out.It feels like '+ body.current.feelslike+ ' on  '+body.current.observation_time+'.The wind speed is '+body.current.wind_speed+' km/h and wind direction is '+body.current.wind_dir+'.The Humidity is '+body.current.humidity+'% .')
             
         }
     })
